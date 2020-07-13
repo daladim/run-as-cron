9f72bb76602400d0d7b6f6a25785b08bcaf89654
daladim <daladim@users.noreply.github.com>
2020-07-13 22:50:37 +0000

    From gists: revision 2
    This includes work from poleguy

--- run-as-cron.sh
+++ run-as-cron.sh
@@ -4,37 +4,75 @@
 #  * with a specific shell, which may or may not be bash
 #  * without an attached input terminal
 #  * in a non-interactive shell
+# This scripts supports cron jobs run by any user, just run it as the target user (e.g. using sudo -u <username>)
+
+# An up-to-date version of this script may be available at https://gist.github.com/daladim/7f67eb95d59aadc8f3e8cd66c6f235d3
 
 function usage(){
     echo "$0 - Run a script or a command as it would be in a cron job, then display its output"
     echo "Usage:"
     echo "   $0 [command | script]"
+    echo ""
+    echo "This scripts supports cron jobs run by any user."
+    echo "To do so, just run it as the target user (e.g. using sudo -u <username> $0)"
 }
 
-if [ "$1" == "-h" -o "$1" == "--help" ]; then
+if [ "$#" -lt 1 -o "$1" == "-h" -o "$1" == "--help" ]; then
     usage
     exit 0
 fi
 
-if [ $(whoami) != "root" ]; then
-    echo "Only root is supported at the moment"
-    exit 1
-fi
+# Depending on the distro, $HOME may or may not be affected by sudo.
+# This is a better way to get the home directory of the current user
+home=$( getent passwd "$(whoami)" | cut -d: -f6 )
+
+cron_env="$home/.config/run-as-cron/cron-env"
+function generate_env_file(){
+    # This function adds /usr/bin/env to a cron job and makes sure it is run only once
+    # i.e. it just helps the user who does not want to do it himself
+    echo -n "Adding a job to cron, to be run once... "
+    generator=$(mktemp /tmp/generator.cron.XXXX)
+    chmod 700 "$generator"
+    cat > "$generator" <<eof
+#!/bin/bash
+    mkdir -p $(dirname "$cron_env")
+    /usr/bin/env > ${cron_env}
+    # Remove this script from the current cron jobs
+    crontab -l | sed "/$(basename $generator)/d" | crontab -
+    rm "$generator"
+eof
+    # Add the just-created-script to cron
+    crontab -l | { cat; echo "* * * * * $generator"; } | crontab -
+    if [ "$?" -eq 0 ]; then
+        echo "Done at $(date)"
+        echo "It will be run shortly. You can try to run $0 again in a minute."
+        return 0
+    else
+        echo "Failed."
+        return 2
+    fi
+}
 
 # This file should contain the cron environment.
-cron_env="/root/cron-env"
 if [ ! -f "$cron_env" ]; then
-    echo "Unable to find $cron_env"
-    echo "To generate it, run \"/usr/bin/env > /root/cron-env\" as a cron job"
-    exit 0
+    echo "Unable to find $cron_env" >&2
+    echo "To generate it, run \"/usr/bin/env > $cron_env\" as a cron job" >&2
+    echo -n "Do you want this script to do it for you? [y/n] "
+    read reply
+    if [ "$reply" == "y" -o "$reply" == "Y" ]; then
+        generate_env_file
+        exit $?
+    fi
+    exit 1
 fi
 
 # It will be a nightmare to expand "$@" inside a shell -c argument.
 # Let's rather generate a string where we manually expand-and-quote the arguments
+# Note that may fail in case arguments (or environment variables) contain quotes...
 env_string="/usr/bin/env -i "
-for envi in $(cat "$cron_env"); do
-   env_string="${env_string} $envi "
-done
+while read -r line; do
+    env_string="${env_string} \"$line\" "
+done < "$cron_env"
 
 cmd_string=""
 for arg in "$@"; do
@@ -42,7 +80,11 @@ for arg in "$@"; do
 done
 
 # Which shell should we use?
-the_shell=$(grep -E "^SHELL=" /root/cron-env | sed 's/SHELL=//')
+the_shell=$(grep -E "^SHELL=" "$cron_env" | sed 's/SHELL=//')
+if [ -z "$the_shell" ]; then
+    echo "Unable to detect what shell should be used for this user." >&2
+    exit 2
+fi
 echo "Running with $the_shell the following command: $cmd_string"
 
 
